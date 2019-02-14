f14c788e56621927801d2969082e829a7ca8c164
saandii4285 <47385988+saandii4285@users.noreply.github.com>
2019-02-14 10:42:52 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,37 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = parseInt(document.getElementById("edad").value);
+
+if( edad >=18 )
+{
+
+alert("es mayor de edad");
+
+}
+
+var edad;
+
+edad = parseInt(document.getElementById("edad").value);
+
+if( edad >= 13 && edad <= 17 )
+
+{
+
+alert("Es adolescente");
+
+}
+
+var edad;
+
+edad = parseInt(document.getElementById("edad").value);
+
+if( edad < 13 )
+{
+    alert("Usted es un niño";)
+}
 
 
 
